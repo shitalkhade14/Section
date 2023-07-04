189bdc399629b8da65c9e633f11486c4db64baba
shitalkhade14 <137171695+shitalkhade14@users.noreply.github.com>
2023-07-04 17:17:21 +0530

    Add files via upload

--- Listbox.js
+++ Listbox.js
@@ -4,6 +4,8 @@ const listbox = document.getElementById("listbox");
 
                 const currentOption = event.target;
 
+                // accessible with arrow keys
+
                 if (event.key === "ArrowUp") {
 
                     event.preventDefault();
@@ -13,21 +15,32 @@ const listbox = document.getElementById("listbox");
                         //accessible with arrow keys arrow up selected
 
 
+
+
                        const previousElement=currentOption.previousElementSibling;
+
+
+
 
                        previousElement.setAttribute("aria-selected","true");
 
                        previousElement.setAttribute("tabindex","0");
 
+
+
+
                        currentOption.setAttribute("aria-selected","false");
 
-                       currentOption.setAttribute("tabindex","-1");
+                    currentOption.setAttribute("tabindex","-1");
+
+
 
 
                        // currentOption.previousElementSibling.focus();
 
                        previousElement.focus();
 
+                       
 
                     } else {
 
@@ -44,6 +57,8 @@ const listbox = document.getElementById("listbox");
                     if (currentOption.nextElementSibling) {
 
 
+
+
                         //accessible with arrow down option is selected
 
                         const nextElement=currentOption.nextElementSibling;
@@ -52,12 +67,18 @@ const listbox = document.getElementById("listbox");
 
                         nextElement.setAttribute("tabindex","0");
 
+
+
+
                         currentOption.setAttribute("aria-selected","false");
 
                         currentOption.setAttribute("tabindex","-1");
 
 
-               nextElement.focus();
+
+
+
+                        nextElement.focus();
 
                     } else {
 
@@ -70,3 +91,110 @@ const listbox = document.getElementById("listbox");
                 }
 
              });
+
+
+
+
+const listbox = document.getElementById("listbox");
+
+ 
+
+listbox.addEventListener("keydown", function (event) {
+
+    const currentOption = event.target;
+
+ 
+
+    // Accessible with arrow keys
+
+    if (event.key === "ArrowUp") {
+
+        event.preventDefault();
+
+        if (currentOption.previousElementSibling) {
+
+            // Accessible with arrow keys arrow up selected
+
+            const previousElement = currentOption.previousElementSibling;
+
+            previousElement.setAttribute("aria-selected", "true");
+
+            previousElement.setAttribute("tabindex", "0");
+
+            currentOption.setAttribute("aria-selected", "false");
+
+            currentOption.setAttribute("tabindex", "-1");
+
+            previousElement.focus();
+
+        } else {
+
+            const lastOption = listbox.lastElementChild;
+
+            lastOption.focus();
+
+        }
+
+    } else if (event.key === "ArrowDown") {
+
+        event.preventDefault();
+
+        if (currentOption.nextElementSibling) {
+
+            // Accessible with arrow down option is selected
+
+            const nextElement = currentOption.nextElementSibling;
+
+            nextElement.setAttribute("aria-selected", "true");
+
+            nextElement.setAttribute("tabindex", "0");
+
+            currentOption.setAttribute("aria-selected", "false");
+
+            currentOption.setAttribute("tabindex", "-1");
+
+            nextElement.focus();
+
+        } else {
+
+            const firstOption = listbox.firstElementChild;
+
+            firstOption.focus();
+
+        }
+
+    }
+
+});
+
+ 
+
+listbox.addEventListener("mousedown", function (event) {
+
+    const clickedOption = event.target;
+
+ 
+
+    // Remove tabindex and aria-selected attributes from all options
+
+    const options = listbox.querySelectorAll("option");
+
+    options.forEach((option) => {
+
+        option.removeAttribute("tabindex");
+
+        option.removeAttribute("aria-selected");
+
+    });
+
+ 
+
+    // Set tabindex and aria-selected attributes for the clicked option
+
+    clickedOption.setAttribute("tabindex", "0");
+
+    clickedOption.setAttribute("aria-selected", "true");
+
+});
+
+             
